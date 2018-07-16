bb50c52aafae21daa026d000dee2e03824b02cd8
DimitrisB <bdimitris@outlook.com>
2018-07-16 15:12:57 +0300

    customer model-1

---
+++ GitDim mvm -test01/GitDim mvm -test01/Models/Customer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitDim_mvm__test01.Models
+{
+    public class Customer : INotifyPropertyChanged
+    {
+
+        /// <summary>
+        /// Initializes a new instance of the Customer class;
+        /// </summary>
+        public Customer(string customerName)
+        {
+            Name = customerName;
+        }
+
+
+        private string _Name;
+        /// <summary>
+        /// Gets or sets the Customer's name.
+        /// </summary>
+        public string Name
+        {
+            get
+            {
+                return _Name;
+            }
+            set
+            {
+                value = _Name;
+                OnpropertyChanged("Name");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
